9b88bd1541fa918d350940b403615a744a712ebd
Simone Saputo <simonesaputo0@gmail.com>
2022-04-28 13:30:00 +0200

    header-first-commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div>
+    
+    <HeaderComp/>
+
+  </div>
+
+</template>
+
+<script>
+import HeaderComp from './components/HeaderComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComp,
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/style/general.scss';
+// @import '../assets/style/general.scss';
+@import url('https://fonts.googleapis.com/css2?family=Fira+Sans+Extra+Condensed&display=swap');
+
+  body {
+    // font-family: 'Fira Sans Extra Condensed', sans-serif;
+  }
+
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,117 @@
+<template>
+
+  <div id="global" class="h-40-vh">
+
+    <div class="container-default d_flex justify_between p_t_20">
+
+        <div>
+          <img src="../assets/img/work-element/dark-logo.png" alt="logo MaxCoach" class="">
+        </div>
+
+        <ul class="listTypeNone d_flex flex_row align_center">
+            <li class="d_flex text_capital flex_row p-x-10 align_center dropdown">
+                <a class="text_decoration p-x-5" href="#">home</a>
+                <i class="fa-solid fa-chevron-down"></i>
+                <div class="dropdown-content">
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                </div>
+            </li>
+            <li class="d_flex text_capital flex_row p-x-10 align_center dropdown">
+                <a class="text_decoration p-x-5" href="#">pages</a>  
+                <i class="fa-solid fa-chevron-down"></i>
+                <div class="dropdown-content">
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                </div>
+            </li>
+            <li class="d_flex text_capital flex_row p-x-10 align_center dropdown">
+                <a class="text_decoration p-x-5" href="#">courses</a>  
+                <i class="fa-solid fa-chevron-down"></i>
+                <div class="dropdown-content">
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                </div>
+            </li>
+            <li class="d_flex text_capital flex_row p-x-10 align_center dropdown">
+                <a class="text_decoration p-x-5" href="#">features</a>  
+                <i class="fa-solid fa-chevron-down"></i>
+                <div class="dropdown-content">
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                </div>
+            </li>
+            <li class="d_flex text_capital flex_row p-x-10 align_center dropdown">
+                <a class="text_decoration p-x-5" href="#">blog</a>  
+                <i class="fa-solid fa-chevron-down"></i>
+                <div class="dropdown-content">
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                </div>
+            </li>
+            <li class="d_flex text_capital flex_row p-x-10 align_center dropdown">
+                <a class="text_decoration p-x-5" href="#">shop</a>  
+                <i class="fa-solid fa-chevron-down"></i>
+                <div class="dropdown-content">
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                </div>
+            </li>
+            <li class="d_flex text_capital flex_row p-x-10 align_center">
+                <i class="fa-solid fa-cart-shopping"></i>
+            </li>
+            <li class="d_flex text_capital flex_row p-x-10 align_center">  
+                <i class="fa-solid fa-user"></i>
+            </li>
+            <li id="search" class="d_flex text_capital flex_row p-x-10 align_center b_rounded_1">
+                <input type="text" placeholder="Search..." class="b_none">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </li>
+        </ul>
+
+    </div>
+      
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'HeaderComp',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/general.scss';
+
+    #global {
+        background-image: url('../assets/img/general/jumbo-overlay.svg');
+    }
+
+    a,
+    i {
+        color: $blue-logo;
+    }
+
+    i {
+        cursor: pointer;
+    }
+
+    #search {
+        background: $white;
+        padding: 15px;
+    }
+ 
+
+</style>
